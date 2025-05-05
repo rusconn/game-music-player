e18eefc4bc80c725a4fe64518d150b262f85b87a
rusconn <rusconn3@gmail.com>
2025-05-06 07:28:24 +0900

    refactor: Customized built-in element → Autonomous custom element

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -3,11 +3,11 @@ import type { MusicPlayerElement } from "../components/music-player.astro.0.mts"
 import type { MusicTabulizerElement } from "../components/music-tabulizer.astro.0.mts";
 
 export const queryMusicPlayer = (): MusicPlayerElement => {
-  return $('div[is="music-player"]')!;
+  return $("music-player")!;
 };
 
 export const queryMusicTabulizer = (): MusicTabulizerElement => {
-  return $('table[is="music-tabulizer"]')!;
+  return $("music-tabulizer")!;
 };
 
 const $ = document.querySelector.bind(document);
