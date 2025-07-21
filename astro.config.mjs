4208f25bfe6146af5c42daf7aefa5c5ac09d215a
rusconn <rusconn3@gmail.com>
2025-07-21 13:39:48 +0900

    feat: support PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,39 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+//@ts-check
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      VitePWA({
+        devOptions: { enabled: true },
+        registerType: "autoUpdate",
+        includeAssets: ["5-seconds-of-silence.mp3", "favicon.svg", "icons.svg"],
+        manifest: {
+          name: "Game Music Player",
+          short_name: "Game Music Player",
+          start_url: "/",
+          display: "standalone",
+          orientation: "portrait",
+          background_color: "#222",
+          theme_color: "#222",
+          icons: [
+            {
+              src: "/icon-192.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+            {
+              src: "/icon-512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
+        },
+      }),
+    ],
+  },
+});
