e1e3911bd62ed2b018cab77f33a1a71b1dcc5816
rusconn <rusconn3@gmail.com>
2026-06-29 18:52:02 +0900

    refactor(pwa): vite-plugin-pwa -> @vite-pwa/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,36 @@
 //@ts-check
 
+import AstroPWA from "@vite-pwa/astro";
 import { defineConfig } from "astro/config";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      VitePWA({
-        devOptions: { enabled: true },
-        registerType: "autoUpdate",
-        includeAssets: ["5-seconds-of-silence.mp3", "favicon.svg", "icons.svg"],
-        manifest: {
-          name: "Game Music Player",
-          short_name: "Game Music Player",
-          start_url: "/",
-          display: "standalone",
-          orientation: "portrait",
-          background_color: "#222",
-          theme_color: "#222",
-          icons: [
-            {
-              src: "/icon-192.png",
-              sizes: "192x192",
-              type: "image/png",
-              purpose: "any maskable",
-            },
-            {
-              src: "/icon-512.png",
-              sizes: "512x512",
-              type: "image/png",
-              purpose: "any maskable",
-            },
-          ],
-        },
-      }),
-    ],
-  },
+  integrations: [
+    AstroPWA({
+      registerType: "autoUpdate",
+      includeAssets: ["5-seconds-of-silence.mp3", "favicon.svg", "icons.svg"],
+      manifest: {
+        name: "Game Music Player",
+        short_name: "Game Music Player",
+        start_url: "/",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#222",
+        theme_color: "#222",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
