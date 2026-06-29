86e0a950262b1074faafaa1e49a18f2458d437a8
rusconn <rusconn3@gmail.com>
2026-06-29 20:31:37 +0900

    chore: rename pwa icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,13 +26,13 @@ export default defineConfig({
         theme_color: "#222",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/pwa-icon-192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/icon-512.png",
+            src: "/pwa-icon-512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
